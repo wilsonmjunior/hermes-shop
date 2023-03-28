02d966a2ce8bb5fc1ddd75de71bea4112419ac20
Wilson Junior <wilson.junior@merx.tech>
2023-03-27 22:35:29 -0300

    feat: initial setup

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,18 @@
+import { GetStaticProps } from "next"
+import { useRouter } from "next/router"
+
+export default function Product() {
+  const { query } = useRouter()
+
+  return (
+    <h1>Product {JSON.stringify(query)}</h1>
+  )
+}
+
+export const getStaticProps: GetStaticProps = (ctx) => {
+  return {
+    props:{
+
+    }
+  }
+}
